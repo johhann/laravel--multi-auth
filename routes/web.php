5528f3b39501ddf3be072a81ef7a322bdb16d049
Yohannes Mekonnen <johannesm1219@gmail.com>
2021-08-26 05:15:44 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/confirmed', function () {
+    return 'password confirmed';
+})->middleware(['auth', 'password.confirm']);
+
+Route::get('/verified', function () {
+    return 'email verified';
+})->middleware('verified');
+
+Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
+    Route::get('/', function () {
+        return view('admin.welcome');
+    })->name('welcome');
+    
+    Auth::routes(['verify' => true]);
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+    
+    Route::get('/confirmed', function () {
+        return 'password confirmed';
+    })->middleware(['auth:admin-web', 'password.confirm:admin.password.confirm']);
+    
+    Route::get('/verified', function () {
+        return 'email verified';
+    })->middleware('verified:admin.verification.notice,admin-web');
+});
